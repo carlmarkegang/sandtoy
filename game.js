9907e8ed838ea4ee2c5651bf1c9d050ea6c8b119
carlmarkegang <huledule@hotmail.com>
2024-07-21 12:00:35 +0200

    more logic

--- game.js
+++ game.js
@@ -40,7 +40,6 @@ function draw() {
         if (mouseIsPressed) {
             if (floor(mouseX) == backgroundPixels[i].x && floor(mouseY) == backgroundPixels[i].y) {
                 backgroundPixels[i].type = "sand"
-
             }
         }
 
@@ -50,9 +49,6 @@ function draw() {
 }
 
 
-function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
 
 
 
@@ -63,23 +59,115 @@ function updatePixels() {
 
     for (let i = 0; i < backgroundPixels.length; i++) {
 
+        if (backgroundPixels[i].type == "sky") {
+            continue;
+        }
+
         if (backgroundPixels[i].updatedInCycle == true) {
             continue;
         }
 
+        var getPixelAround = getPixelsAround(backgroundPixels[i]);
+        var pixelUnder = getPixelAround[0];
+        var pixelLeft = getPixelAround[1];
+        var pixelRight = getPixelAround[2];
+        var pixelUnderLeft = getPixelAround[3];
+        var pixelUnderRight = getPixelAround[4];
+
+
         if (backgroundPixels[i].type == "sand") {
-            for (let i2 = 0; i2 < backgroundPixels.length; i2++) {
-                if ((backgroundPixels[i].y + 1) == backgroundPixels[i2].y && backgroundPixels[i].x == backgroundPixels[i2].x) {
-                    if (backgroundPixels[i2].type == "sky") {
-                        backgroundPixels[i2].type = "sand";
-                        backgroundPixels[i].type = "sky";
-                        backgroundPixels[i2].updatedInCycle = true;
-                    }
-                }
+            if (pixelUnder.type == "sky") {
+                pixelUnder.type = "sand";
+                backgroundPixels[i].type = "sky";
+                pixelUnder.updatedInCycle = true;
+                backgroundPixels[i].updatedInCycle = true;
+                continue;
             }
+
+            if (pixelUnder.type == "sand" && pixelLeft.type == "sky" && pixelUnderLeft.type == "sky") {
+                pixelUnderLeft.type = "sand";
+                backgroundPixels[i].type = "sky";
+                pixelUnderLeft.updatedInCycle = true;
+                backgroundPixels[i].updatedInCycle = true;
+                continue;
+            }
+
+
+            if (pixelUnder.type == "sand" && pixelRight.type == "sky" && pixelUnderRight.type == "sky") {
+                pixelUnderRight.type = "sand";
+                backgroundPixels[i].type = "sky";
+                pixelUnderRight.updatedInCycle = true;
+                backgroundPixels[i].updatedInCycle = true;
+                continue;
+            }
+
         }
     }
 
+    return;
+
 }
 
-myInterval = setInterval(updatePixels, 60);
+function getPixelsAround(backgroundPixel) {
+    var PixelArray = [];
+
+    for (let i2 = 0; i2 < backgroundPixels.length; i2++) {
+        if (backgroundPixel.updatedInCycle == true) {
+            continue;
+        }
+
+        if (backgroundPixels[i2].updatedInCycle == true) {
+            continue;
+        }
+
+        if ((backgroundPixel.y + 1) == backgroundPixels[i2].y && backgroundPixel.x == backgroundPixels[i2].x) {
+            if (backgroundPixels[i2].updatedInCycle == false) {
+                PixelArray[0] = backgroundPixels[i2];
+            }
+        }
+
+        if (backgroundPixel.y == backgroundPixels[i2].y && (backgroundPixel.x - 1) == backgroundPixels[i2].x) {
+            if (backgroundPixels[i2].updatedInCycle == false) {
+                PixelArray[1] = backgroundPixels[i2];
+            }
+        }
+
+        if (backgroundPixel.y == backgroundPixels[i2].y && (backgroundPixel.x + 1) == backgroundPixels[i2].x) {
+            if (backgroundPixels[i2].updatedInCycle == false) {
+                PixelArray[2] = backgroundPixels[i2];
+            }
+        }
+
+        if ((backgroundPixel.y + 1) == backgroundPixels[i2].y && (backgroundPixel.x - 1) == backgroundPixels[i2].x) {
+            if (backgroundPixels[i2].updatedInCycle == false) {
+                PixelArray[3] = backgroundPixels[i2];
+            }
+        }
+
+        if ((backgroundPixel.y + 1) == backgroundPixels[i2].y && (backgroundPixel.x + 1) == backgroundPixels[i2].x) {
+            if (backgroundPixels[i2].updatedInCycle == false) {
+                PixelArray[4] = backgroundPixels[i2];
+            }
+        }
+
+        if (PixelArray[0] != undefined && PixelArray[1] != undefined && PixelArray[2] != undefined && PixelArray[3] != undefined && PixelArray[4] != undefined) {
+            continue;
+        }
+    }
+
+    for (let i2 = 0; i2 < 5; i2++) {
+        if (PixelArray[i2] == undefined) {
+            PixelArray[i2] = [];
+        }
+    }
+
+    return PixelArray;
+}
+
+
+myInterval = setInterval(updatePixels, 100);
+
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
